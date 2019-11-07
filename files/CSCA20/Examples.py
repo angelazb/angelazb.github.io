929c0449273aacd0ebc59eddbec76d94c0531d52
Angela Zavaleta <angelazb@Angelas-MacBook-Pro.local>
2019-11-07 15:00:34 -0500

    Added A20 stuff

---
+++ files/CSCA20/Examples.py
@@ -0,0 +1,72 @@
+######################
+# uoft.me/a20-lab7
+######################
+
+######################
+a = 3
+b = "3"
+#print(a == b)
+#print(a == int(b))
+#print(str(a) == b)
+
+######################
+word = "hello"
+letter = "e"
+#if letter in word:
+    #print("yes")
+#else:
+    #print("no")
+
+######################
+word = "hello"
+#for letter in word:
+    #print(letter)
+
+#######################
+word = "hello"
+#for index in range(len(word)):
+    #print(word[index])
+    
+#######################
+index = 0
+word = "hello"
+#while index < len(word):
+    #print(word[index])
+    #index += 1
+
+#######################
+list_letters = ['a', 'b', 'c', 'd']
+
+#for letter in list_letters:
+    #print(letter)
+
+list_letters.append('e')
+#print(list_letters)
+
+list_letters.remove('d')
+#print(list_letters)
+
+#list_letters.insert(1, 'Y')
+#print(list_letters)
+
+#list_letters[1] = 'X'
+#print(list_letters)
+
+#list_letters.clear()
+#print(list_letters)
+
+list_numbers = []
+for index in range(5):
+    list_numbers.append(index)
+#print(list_numbers)
+
+for index in range(5, 10):
+    list_numbers.append(index)
+#print(list_numbers)
+#print(list_numbers[3:6])
+#print(list_numbers[:-1])
+
+#######################
+word = "Angelaa"
+#print(word[1:3])
+#print(word[:-1])
